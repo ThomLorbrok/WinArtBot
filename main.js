15caff199ee4d69da00b2d06c30ccb7d4d40876a
ThomLorbrok <38364156+ThomLorbrok@users.noreply.github.com>
2018-05-22 17:33:16 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,47 @@
+const Discord = require('discord.js');
+
+var bot = new Discord.Client();
+var prefix = ("-");
+
+bot.on('ready', () => {
+    console.log("Prêt à travailler chef ");
+});
+
+bot.login(process.env.TOKEN);
+
+bot.on('message', message => {
+    if (message.content === "ping"){
+        message.reply("pong");
+        console.log('ping pong');
+    }
+
+    if (message.content === prefix + "help"){
+        message.channel.sendMessage("Un autre staff va mieux vous aider");
+        console.log("Rigolo");
+    }
+    
+    if (message.content === prefix + "help"){
+        message.channel.sendMessage("Je rigole voilà les commandes : -help , -Google , -infobot , -clear");
+        console.log("Help");
+    }
+
+    if (message.content === prefix + "Google"){
+        message.channel.sendMessage("C'est un navigateur le plus utilisé en France");
+        console.log("Commande en cours ");
+    }
+
+    if (message.content === prefix + "infobot"){
+        message.channel.sendMessage("Il a été crée et développer par thom avec ses plus grands soins !");
+        console.log("Commande Validé");
+    }
+    
+   if(message.content === prefix + "clear"){
+            if (message.member.hasPermission("MANAGE_MESSAGES")){
+                message.channel.fetchMessages()
+                    .then(function(list){
+                        message.channel.bulkDelete(list);
+                    }, function(err){message.channel.send("Erreur")})
+            message.channel.send('Vous avez réussie à vider le salon.')}
+                    console.log("La commande clear viens d'être effectué par un membre de l'équipe.")
+    }
+})
